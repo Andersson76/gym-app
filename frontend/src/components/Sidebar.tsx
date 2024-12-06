3aa1236d842038a486feba6efaa8a56299acf8c4
Martin Andersson <martin.andersson@me.com>
2024-12-06 15:21:49 +0100

    Created workouts and settings components and arranged layout

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="w-full md:w-1/4 bg-blue-900 p-4">
+    <aside className="w-full md:w-1/4 bg-blue-900 p-4 rounded-3xl my-3 mx-3">
       <nav>
         <ul>
           <li>
-            <Link href="/" className="text-white block py-2">
+            <Link href="/" className="text-white text-2xl block py-2">
               Dashboard
             </Link>
           </li>
           <li>
-            <Link href="/workouts" className="text-white block py-2">
+            <Link href="/workouts" className="text-white text-2xl block py-2">
               Workouts
             </Link>
           </li>
           <li>
-            <Link href="/settings" className="text-white block py-2">
+            <Link href="/settings" className="text-white text-2xl block py-2">
               Settings
             </Link>
           </li>
