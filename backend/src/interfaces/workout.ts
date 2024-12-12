eb5334e71d4b9c3bc04b7d1ac2e0f432cffd295e
Martin Andersson <martin.andersson@me.com>
2024-12-12 16:27:15 +0100

    Added error handling and cleaning code

--- backend/src/interfaces/workout.ts
+++ backend/src/interfaces/workout.ts
@@ -1,8 +1,5 @@
 export interface Workout {
   id: number;
-  /*name: string;
-  duration: number;
-  type: string; */
   title: string;
   description: string;
   date: string;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,11 @@ import app from "./app";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+if (!process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGDATABASE) {
+  console.error("Missing required environment variables");
+  process.exit(1);
+}
+
 const PORT = Number(process.env.PORT) || 5004;
 const HOST = "0.0.0.0";
 
