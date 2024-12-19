ba22106d01bcb92d26bf080cae8d3ad14e1def1b
Martin Andersson <martin.andersson@me.com>
2024-12-19 10:39:34 +0100

    Change text and clean up

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -3,8 +3,8 @@ import Settings from "@/components/Settings";
 export default function Workouts() {
   return (
     <div className="p-6 text-blue-500">
-      <h1 className="text-2xl font-bold">Workouts</h1>
-      <p className="text-gray-600">Here you can manage your workout plans.</p>
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <p className="text-gray-600">Here you can toggle your settings.</p>
       <Settings />
     </div>
   );
