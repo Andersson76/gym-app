d29317ab7d10572d2ffaf8fcbd17e868cd286415
Martin Andersson <martin.andersson@me.com>
2024-12-21 22:54:22 +0100

    Problem with post in form

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "cypress";
+import webpackConfig from "./cypress/webpack.config";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: "http://localhost:3000",
+    env: {
+      NEXT_PUBLIC_API_URL: "http://localhost:5004",
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+      webpackConfig,
+    },
   },
 });
---
+++ cypress/webpack.config.ts
@@ -0,0 +1,28 @@
+import { Configuration } from "webpack";
+
+const webpackConfig: Configuration = {
+  resolve: {
+    alias: {
+      "node:test": false,
+    },
+    fallback: {
+      fs: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+export default webpackConfig;
